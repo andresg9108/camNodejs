587c693f0d74103e2ab3b4fc4258c20336d4c422
Andres F. Gonzalez <ag2018ci24@gmail.com>
2018-03-27 19:06:34 -0500

    Guardando cambios.

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var app = new express();
 var http = require("http").Server(app);
-var io = require('ocket.io')(http);
+var io = require('socket.io')(http);
+
+var log = require('log'),
+	log = new log('debug');
 
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/', function(req, res){
+	res.redirect('index.html');
+});
+
+io.on('connection', function(socket){
+	
+});
+
 app.listen(port, function(){
+	log.info("Puerto: " + port);
 	console.log("Puerto: " + port);
 });
